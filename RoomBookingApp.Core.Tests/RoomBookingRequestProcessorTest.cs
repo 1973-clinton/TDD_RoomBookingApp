c955f5a8cbe1aa47b1da6b24c2f49ac42ee469ef
kington <clintonboamahofficial@gmail.com>
2022-07-31 17:24:05 +0000

    completed first unit test on room booking application

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,41 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace RoomBookingApp.Core.Tests
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void ShouldReturnRoomBookingRequest()
+        {
+            //Arrange
+            var request = new RoomBookingRequest
+            {
+                FullName = "Clinton Test",
+                Email = "clinton@kington.com",
+                Date = new DateTime(2022, 07, 24)
+            };
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+            //Assertion using 
+            result.ShouldNotBeNull();
+            result.FullName.ShouldBe(request.FullName);
+            result.Email.ShouldBe(request.Email);
+            result.Date.ShouldBe(request.Date);
+
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,22 @@
+﻿using RoomBookingApp.Core.Models;
+using System;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingRequest request)
+        {
+            return new RoomBookingResult
+            {
+                Date = request.Date,
+                FullName = request.FullName,
+                Email = request.Email,
+            };
+        }
+    }
+}
